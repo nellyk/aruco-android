f567b98e789f9a519e56a5abcfe978711040da72
rafaelmiguel.ortega <rafaelmiguel.ortega@gmail.com>
2012-04-09 21:37:08 +0000

    Board detection implemented and several 3d models supported

--- Aruco/src/es/ava/aruco/BoardConfiguration.java
+++
@@ -1,7 +0,0 @@
-package es.ava.aruco;
-
-public class BoardConfiguration{
-	protected int width, height;
-	protected int[][] markersId;// TODO en aruco viene como Mat pero no es necesario ¿?
-	protected int markerSizePix, markerDistancePix;
-}
--- Aruco/src/es/ava/aruco/BoardDetector.java
+++ Aruco/src/es/ava/aruco/BoardDetector.java
@@ -8,8 +8,6 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 
-import es.ava.aruco.Board.BoardConfiguration;
-
 public class BoardDetector {
 	
 	public float detect(Vector<Marker> detectedMarkers, BoardConfiguration conf, Board bDetected,
@@ -20,10 +18,11 @@ public class BoardDetector {
 	
 	private float detect(Vector<Marker> detectedMarkers, BoardConfiguration conf, Board bDetected,
 			Mat camMatrix, Mat distCoeffs, float markerSizeMeters) throws CvException{
+		bDetected.clear();
 		// find among the detected markers those who belong to the board configuration
 		int height = conf.height;
 		int width = conf.width;
-		int[][] detected = new int[width][height];// indices of the markers
+		int[][] detected = new int[width][height];// indices of the markers in the vector detectedMarkers
 		for(int i=0;i<width;i++)
 			for(int j=0;j<height;j++)
 				detected[i][j] = -1;
@@ -35,7 +34,7 @@ public class BoardDetector {
 	        for(int j=0;j<height && ! found;j++)
 	            for(int k=0;k<width && ! found;k++)
 				if(conf.markersId[j][k] == id){
-					detected[j][k] = id;
+					detected[j][k] = i;
 					nMarkInBoard++;
 					found = true;
 					bDetected.add(detectedMarkers.get(i));
@@ -57,36 +56,34 @@ public class BoardDetector {
 			int currIndex = 0;
 	        for(int y=0;y<height;y++)
 	            for(int x=0;x<width;x++) {
-				if(detected[y][x] != -1){
-					Vector<Point> points = detectedMarkers.get(detected[y][x]);
-					// set image points
-					double[] buffer = new double[8];
-					for(int p=0;p<4;p++){
-						buffer[(currIndex+p)*2] = points.get(p).x;
-						buffer[(currIndex+p)*2+1] = points.get(p).y;
+					if(detected[y][x] != -1){
+						Vector<Point> points = detectedMarkers.get(detected[y][x]);
+						// set image points
+						double[] buffer = new double[8];
+						for(int p=0;p<4;p++){
+							buffer[p*2] = points.get(p).x;
+							buffer[p*2+1] = points.get(p).y;
+						}
+						imgPoints.put(currIndex, 0, buffer);
+						
+						// translation to put the origin in the center
+	                    float TX=-(  ((detected.length-1)*(markerDistanceMeters+markerSizeMeters) +markerSizeMeters)/2);
+	                    float TY=-(  ((detected.length-1)*(markerDistanceMeters+markerSizeMeters) +markerSizeMeters)/2);
+	                    //points in real reference system. We see the center in the bottom-left corner
+	                    float AY=x*(markerDistanceMeters+markerSizeMeters ) +TY;
+	                    float AX=y*(markerDistanceMeters+markerSizeMeters ) +TX;
+	                    objPoints.put(currIndex, 0, 
+	                    		AX,                  AY,                  0,
+	                    		AX,                  AY+markerSizeMeters, 0,
+	                    		AX+markerSizeMeters, AY+markerSizeMeters, 0,
+	                    		AX+markerSizeMeters, AY,                  0);
+	                    currIndex+=4;
 					}
-					imgPoints.put(0, 0, buffer);
-					
-					// translation to put the origin in the center
-                    float TX=-(  ((detected.length-1)*(markerDistanceMeters+markerSizeMeters) +markerSizeMeters)/2);
-                    float TY=-(  ((detected.length-1)*(markerDistanceMeters+markerSizeMeters) +markerSizeMeters)/2);
-                    //points in real refernce system. We se the center in the bottom-left corner
-                    float AY=x*(markerDistanceMeters+markerSizeMeters ) +TY;
-                    float AX=y*(markerDistanceMeters+markerSizeMeters )+TX;
-                    objPoints.put(0, 0, 
-                    		AX,                  AY,                  0,
-                    		AX,                  AY+markerSizeMeters, 0,
-                    		AX+markerSizeMeters, AY+markerSizeMeters, 0,
-                    		AX+markerSizeMeters, AY,                  0);
-                    currIndex+=4;
-				}
 			}
+	        // TODO get the opencv calls out of the loops
 	        Calib3d.solvePnP(objPoints, imgPoints, camMatrix, distCoeffs, bDetected.Rvec, bDetected.Tvec);
+//	        Utils.rotateXAxis(bDetected.Rvec); rotated later, in getModelViewMatrix
 		}
-		return (nMarkInBoard/( conf.width*conf.height));
-	}
-	
-	private void rotateXAxis(Mat rotation){
-		
+		return ((float)nMarkInBoard/(float)(conf.width*conf.height));
 	}
 }
