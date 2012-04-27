be5ae084f3cc478cb6f26579474b1f0a90e8f8a1
rafaelmiguel.ortega <rafaelmiguel.ortega@gmail.com>
2012-04-27 16:56:51 +0000

    some touch interaction added

--- AndroidSampleProject/src/es/ava/aruco/sample/BoardDetectActivity.java
+++ AndroidSampleProject/src/es/ava/aruco/sample/BoardDetectActivity.java
@@ -21,6 +21,33 @@ public class BoardDetectActivity extends Aruco3dActivity {
 	private AnimationObject3d ogre;
 	
 	@Override
+	public void initDetectionParam() {
+		int[] ids = {200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224};
+		int[][] markersId = new int[5][5];
+		int index = 0;
+		for(int i=0;i<5;i++)
+			for(int j=0;j<5;j++){
+				markersId[i][j] = ids[index];
+				index++;
+			}
+		mShowFps = false;
+		mLookForBoard = true;
+		mBC = new BoardConfiguration(5,5,markersId,100,20);
+		mCamParam = new CameraParameters();
+		mMarkerSize = 0.034f;
+	}
+
+	@Override
+	public void updateScene() {
+
+	}
+
+	@Override
+	public void onDetection(Mat frame, Vector<Marker> detectedMarkers, int idSelected) {
+		
+	}
+	
+	@Override
 	public void initScene() {
 		super.onInitScene();
 		scene.lights().add(new Light());
@@ -36,32 +63,6 @@ public class BoardDetectActivity extends Aruco3dActivity {
 		scene.addChild(ogre);
 		ogre.setFps(50);
 		ogre.play();
-	}
-
-	@Override
-	public void updateScene() {
-
-	}
-
-	@Override
-	public void initDetectionParam() {
-		int[] ids = {200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224};
-		int[][] markersId = new int[5][5];
-		int index = 0;
-		for(int i=0;i<5;i++)
-			for(int j=0;j<5;j++){
-				markersId[i][j] = ids[index];
-				index++;
-			}
-		lookForBoard = true;
-		mBC = new BoardConfiguration(5,5,markersId,100,20);
-		mCamParam = new CameraParameters();
-		mMarkerSize = 0.034f;
-	}
-
-	@Override
-	public void onDetection(Mat frame, Vector<Marker> detectedMarkers) {
-		
 	}
 
 	@Override
